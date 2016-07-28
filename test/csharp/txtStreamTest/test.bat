c19e12d59cf350e32eef9eb8b7746ac946246307
qualiu <qualiu@microsoft.com>
2016-07-29 00:10:51 +0800

    Add x64 configuration for test RIOSocket

--- test/csharp/txtStreamTest/test.bat
+++ test/csharp/txtStreamTest/test.bat
@@ -1,10 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
+echo ### you can set TestExePath to avoid decttion ###
+echo ### you can set HasRIO=1 to enable RIO socket ###
 set ShellDir=%~dp0
 IF %ShellDir:~-1%==\ SET ShellDir=%ShellDir:~0,-1%
 
-for /f %%g in (' for /R %ShellDir% %%f in ^( *.exe ^) do @echo %%f ^| findstr /I /C:vshost /V ^| findstr /I /C:obj /V ') do set ExePath=%%g
-for %%a in ("%ExePath%") do ( 
+if "%TestExePath%"=="" for /f %%g in (' for /R %ShellDir% %%f in ^( *.exe ^) do @echo %%f ^| findstr /I /C:vshost /V ^| findstr /I /C:obj /V ') do set TestExePath=%%g
+for %%a in ("%TestExePath%") do ( 
     set ExeDir=%%~dpa
     set ExeName=%%~nxa
 )
@@ -15,13 +17,13 @@ set CommonToolDir=%ShellDir%\..\..\tools
 call %CommonToolDir%\set-sparkCLR-env.bat %CodeRootDir% || exist /b 1
 
 call :CheckExist %SPARKCLR_HOME%\scripts\sparkclr-submit.cmd "sparkclr-submit.cmd" || exit /b 1
-call :CheckExist %ExePath% "ExePath" || exit /b 1
+call :CheckExist %TestExePath% "TestExePath" || exit /b 1
 call :CheckExist %ExeDir% "ExeDir" || exit /b 1
 
 set AllArgs=%*
 if "%1" == "" (
-    echo No parameter, Usage as following, run : %ExePath%
-    call %ExePath%
+    echo No parameter, Usage as following, run : %TestExePath%
+    call %TestExePath%
     exit /b 0
 )
 
@@ -36,7 +38,7 @@ set options=%options% --conf "spark.yarn.executor.memoryOverhead=18000"
 set options=%options% --conf spark.streaming.kafka.maxRetries=20
 set options=%options% --conf spark.mobius.streaming.kafka.CSharpReader.enabled=true
 set options=%options% --jars %CodeRootDir%\build\dependencies\spark-streaming-kafka-assembly_2.10-1.6.1.jar
-set options=%options% --conf spark.mobius.CSharp.socketType=Rio
+if "%HasRIO%" == "1" set options=%options% --conf spark.mobius.CSharp.socketType=Rio
 
 call %SPARKCLR_HOME%\scripts\sparkclr-submit.cmd %options% --exe %ExeName% %CD% %AllArgs%
 
